54d31df268faf4d0d7208c4dda229cc77943d242
sonu-dev <sonu.econnect@gmail.com>
2026-02-22 16:27:50 +0530

    docker deploy

--- Agents/AI-Coder/llm.py
+++ Agents/AI-Coder/llm.py
@@ -5,8 +5,8 @@ def get_llm():
     
     llm = ChatOllama(
         model="qwen2.5-coder:7b",
-        temperature=0,
-        debug=True
+        base_url="http://host.docker.internal:11434",  # important
+        temperature=0.1,
     )
     
     return llm
